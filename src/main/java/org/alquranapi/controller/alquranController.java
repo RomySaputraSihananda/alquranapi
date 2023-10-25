1f023121bf669680a743f4d78808688d55c5bb5d
romys <romysaputrasihanandaa@gmail.com>
2023-10-25 23:14:08 +0700

    update controller

--- src/main/java/org/alquranapi/controller/alquranController.java
+++ src/main/java/org/alquranapi/controller/alquranController.java
@@ -1,8 +1,7 @@
 package org.alquranapi.controller;
 
-import org.alquranapi.Model.DTO.SuratDetailDTO;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.alquranapi.service.AlquranService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @RestController
 @RequestMapping("/api/v1/alquran")
 public class alquranController {
+
+    @Autowired
+    private AlquranService alquranService;
+
     @GetMapping
     public ResponseEntity<?> handler(@RequestParam(name = "nomorSurat") int nomor) {
-
+        return new ResponseEntity<>(this.alquranService.getDetail(nomor), HttpStatus.OK);
     }
 }
