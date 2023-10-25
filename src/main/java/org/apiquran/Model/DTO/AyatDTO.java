e7872e70492f774928d0ccc74073961fc3aea01a
romys <romysaputrasihanandaa@gmail.com>
2023-10-25 18:38:01 +0700

    first commit

---
+++ src/main/java/org/apiquran/Model/DTO/AyatDTO.java
@@ -0,0 +1,10 @@
+package org.apiquran.Model.DTO;
+
+import lombok.Data;
+
+@Data
+public class AyatDTO {
+    private String teksArab;
+    private String teksLatin;
+    private String teksIndonesia;
+}
---
+++ src/test/java/org/apiquran/apiquran/ApiquranApplicationTests.java
@@ -0,0 +1,13 @@
+package org.apiquran.apiquran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiquranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
