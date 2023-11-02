03d8674066f01632d190c3e66e228eb9b0a32a7f
romys <romysaputrasihanandaa@gmail.com>
2023-11-02 11:19:24 +0700

    add tafsir.sql

---
+++ src/main/java/org/alquranapi/Model/DTO/AudioDTO.java
@@ -0,0 +1,12 @@
+package org.alquranapi.Model.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AudioDTO {
+    private String audioLink;
+}
